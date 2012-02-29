ba85d2fde2ddbfd180b7dcb38902c517718e7f6a
Fábio Souza <fsouza@fsouza-virtual.(none)>
2012-02-29 17:26:03 -0300

    Script de Instalação

--- setup.sh
+++ setup.sh
@@ -3,15 +3,16 @@
 #Esse script deve ser executado com SUDO
 
 #Instalando o Django 1.3.1
+apt-get install python-dev python-setuptools build-essential
 easy_instal pip
 pip install django==1.3.1
 
 #Instalando o POSTGRE 8.4
-apt-get install postgresql-8.4
-apt-get install python-pgsql
+#apt-get install postgresql-8.4
+#apt-get install python-pgsql
 
 #Configurando o Sistema do Ditex
-chmod 777 /opt
-cp -a tpcavancados /opt
-sudo -u postgres createdb BancoDitex
-python /opt/tpcavancados/manage.py syncdb
+#chmod 777 /opt
+#cp -a tpcavancados /opt
+#sudo -u postgres createdb BancoDitex
+#python /opt/tpcavancados/manage.py syncdb
